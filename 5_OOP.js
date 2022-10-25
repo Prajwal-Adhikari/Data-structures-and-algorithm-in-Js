b2aafffac8dbb1ae8267ecd965059579a4c3b222
Prajwal-Adhikari <prazolppp@gmail.com>
2022-10-25 14:04:08 +0545

    OOP in js

--- 5_OOP.js
+++ 5_OOP.js
@@ -1,36 +1,76 @@
-//OBJECTS
-const circle = {
-    radius:1,   //property
-    location:{
-        x:1,
-        y:1,
-    },  
-    draw:()=>console.log("circle")  //method
-}
+// //OBJECTS
+// const circle = {
+//     radius:1,   //property
+//     location:{
+//         x:1,
+//         y:1,
+//     },  
+//     draw:()=>console.log("circle")  //method
+// }
 
-circle.draw();
-console.log(circle.radius);
+// circle.draw();
+// console.log(circle.radius);
 
-//Factory function
-function createCircle(radius) {
-    return{
-        radius,
-        draw:()=>console.log(`"circle of radius "`)
-    }
-}
+// //Factory function
+// function createCircle(radius) {
+//     return{
+//         radius,
+//         draw:()=>console.log(`"circle of radius "`)
+//     }
+// }
 
-const newCircle = createCircle(10);
+// const newCircle = createCircle(10);
 
-//constructors
-function Circle(radius){
-    this.radius = radius;
-    this.draw = () => {console.log(`draw circle of ${radius} radius`);
-}
+// //constructors
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = () => {console.log(`draw circle of ${radius} radius`);
+// }
     
+// }
+
+// const another = new Circle(5);
+// another.draw();
+
+// const testCircle = Circle.call({},1);
+
+// //constructor property
+//  console.log(Number())
+//  console.log(String())
+//  console.log(Boolean())
+//  console.log(another.constructor);
+//  console.log(newCircle.constructor);
+
+//values and references
+//value type
+let x = 10;
+console.log(`x is ${x}`);
+let y = x;
+x = 20;
+console.log(`x is ${x}`);
+console.log(`y is ${y}`);
+// x and y are completely independent 
+// when we do y = x, the value stored in the x is COPIED into the y and when we do x = 20, x gets changed to 20 but y is uneffected.
+//reference type
+let a = {value:10}; 
+let b = a;
+console.log(`a is ${a.value}`);
+console.log(`b is ${b.value}`);
+// a and b points to same memory location so when we change a variable, change is reflected into the other.
+
+//example
+let num = 10;
+function increase(num){
+    return num++;
 }
+console.log(increase(num));
+//num is not increased as its a primittive and inside increase function we are incrementing a completely different variable.
 
-const another = new Circle(5);
-another.draw();
-
-//constructor property
- 
+//now lets take a reference type
+let number = {value:10};
+function increment(number){
+    return number.value++;
+}
+increment(number);
+console.log(number);
+//as we are now dealing with a reference type variable we see increment in value of number object when we run it through the increment function.
