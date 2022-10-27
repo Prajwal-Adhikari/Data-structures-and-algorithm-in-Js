ca063710758207bcc6a2e3c61135a3b11be41fe5
Prajwal-Adhikari <prazolppp@gmail.com>
2022-10-27 21:25:32 +0545

    Prepend in linkedlist

--- 8_LinkedList.js
+++ 8_LinkedList.js
@@ -17,8 +17,21 @@ class LinkedList{
     getSize(){
         return this.size;
     }
+    prepend(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
 }
 
 const list = new LinkedList();
 console.log(`List is initially empty : ${list.isEmpty()}`);
 console.log(`Initially the size of the linkedlist is : ${list.getSize()}`);
+console.log("Prepend operation");
+list.prepend(5);
+console.log(list);
