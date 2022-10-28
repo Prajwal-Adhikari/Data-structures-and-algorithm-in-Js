2bf15d88776636bc486bf597e55c9f6e3f2aa44c
Prajwal-Adhikari <prazolppp@gmail.com>
2022-10-28 20:05:39 +0545

    LinkedList print in js

--- 8_LinkedList.js
+++ 8_LinkedList.js
@@ -27,16 +27,32 @@ class LinkedList{
         }
         this.size++;
     }
+    print(){
+        if(list.isEmpty()){
+            console.log("List is empty");
+        }else{
+            let curr = this.head;
+            let listValues = '';
+            while(curr){
+                listValues += `${curr.value} `;
+                curr = curr.next;
+            }
+            console.log(listValues);
+        }
+    }
 }
 
 const list = new LinkedList();
 console.log(`List is initially empty : ${list.isEmpty()}`);
 console.log(`Initially the size of the linkedlist is : ${list.getSize()}`);
+console.log("Printing the list");
+list.print();
 console.log("Prepend operation");
 list.prepend(5);
 list.prepend(50);
 list.prepend(57);
 list.prepend(54);
 list.prepend(5);
-console.log(list);
+list.print();
+
 console.log(list.getSize());
